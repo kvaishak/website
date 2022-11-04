274c28ac0c88b5a1736c395b009df5e00fa05565
kvaishak <kvaishak47@gmail.com>
2022-11-04 19:29:14 +0530

    Mobile Navigation menu

--- components/Menu/menu.js
+++ components/Menu/menu.js
@@ -27,52 +27,83 @@ export default function Menu() {
     },
   ];
 
-  return (
-    <div className={styles.container}>
-      <Link href="/">
-        <div className={styles.homeContainer}>
-          shak{" "}
-          <Image
-            priority
-            className={iconClassName}
-            src={`/icons/Corner.svg`}
-            height={20}
-            width={20}
-            alt="Theme-changer"
-            // onClick={clickHander}
-          />
-        </div>
-      </Link>
-      <div className={styles.itemsContainer}>
-        {navMenuItems.map((navMenu) => (
-          <Link key={navMenu.path} href={navMenu.path}>
-            <a
-              className={
-                navMenu.path === router.pathname ? styles.selected : ""
-              }
-            >
-              {navMenu.label}
-            </a>
-          </Link>
-        ))}
-      </div>
-      <div className={styles.itemsContainer}>
-        <div className={styles.menuButton}>
-          <span>Let&apos;s chat</span>
-        </div>
-        <Theme />
-      </div>
+  const closeMobileMenu = () => {
+    document.body.style.overflow = "visible";
+    setClick(false);
+  };
 
-      <div className={styles.MenuIconContainer}>
-        <Image
-          priority
-          className={iconClassName}
-          src={`/icons/Menu.svg`}
-          height={20}
-          width={20}
-          alt="Theme-changer"
-          // onClick={clickHander}
-        />
+  const handleClick = () => {
+    let overflowValue = click ? "visible" : "hidden";
+    document.body.style.overflow = overflowValue;
+    setClick(!click);
+  };
+
+  if (width > 700 && click) {
+    closeMobileMenu();
+  }
+
+  const navBtnClick = () => {
+    if (click) {
+      closeMobileMenu();
+    }
+  };
+
+  const navigationContainerStyle = `${styles.container} ${
+    click ? styles.click : ""
+  }`;
+
+  return (
+    <div className={navigationContainerStyle}>
+      <div className={styles.navContainer}>
+        <div className={styles.homeContainer}>
+          <Link href="/">
+            <div className={styles.logoContainer}>
+              shak{" "}
+              <Image
+                priority
+                className={iconClassName}
+                src={`/icons/Corner.svg`}
+                height={20}
+                width={20}
+                alt="Theme-changer"
+                // onClick={clickHander}
+              />
+            </div>
+          </Link>
+          <div className={styles.MenuIconContainer} onClick={handleClick}>
+            <Image
+              priority
+              className={iconClassName}
+              src={`/icons/Menu.svg`}
+              height={20}
+              width={20}
+              alt="Theme-changer"
+              // onClick={clickHander}
+            />
+          </div>
+        </div>
+
+        <div className={styles.itemsContainer}>
+          {navMenuItems.map((navMenu) => (
+            <div key={navMenu.path} onClick={navBtnClick}>
+              <Link href={navMenu.path}>
+                <a
+                  className={
+                    navMenu.path === router.pathname ? styles.selected : ""
+                  }
+                >
+                  {navMenu.label}
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className={styles.itemsContainer}>
+          <div className={styles.menuButton}>
+            <span>Let&apos;s chat</span>
+          </div>
+          <Theme />
+        </div>
       </div>
     </div>
   );
