b40aaac953230698c3b6e13c520839d4ff1b0fac
kvaishak <kvaishak47@gmail.com>
2022-12-25 12:21:29 +0530

    Testing

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,16 @@ import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 import Menu from "../components/Menu/menu";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" value={{ dark: "dark-theme" }}>
+      <Head>
+        <meta name="description" content="Hello there" />
+        <meta name="og:title" content="I am a title" />
+        <meta name="og:description" content="I am a description" />
+      </Head>
       <Toaster
         toastOptions={{
           duration: 1500,
